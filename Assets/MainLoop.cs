a208cdb87ad6efaa289d1cbc9833e1f047b177ed
BergWerkGIS <wb@BergWerk-GIS.at>
2017-10-03 16:13:47 +0200

    pass value back and create GameObject

--- Assets/MainLoop.cs
+++ Assets/MainLoop.cs
@@ -6,6 +6,7 @@ public class MainLoop : MonoBehaviour
 {
 
 	private bool _firstUpdate = true;
+	private int? _result = null;
 	private HeavyDutyWorker _hdWorker;
 
 	void Start()
@@ -24,18 +25,27 @@ public class MainLoop : MonoBehaviour
 			_firstUpdate = false;
 
 			//those 3 together take around 5 to 6 secs on my laptop
-			Debug.Log("30: " + SimulateTileCreation.NthFibonacciNumber(30));
-			Debug.Log("35: " + SimulateTileCreation.NthFibonacciNumber(35));
-			Debug.Log("40: " + SimulateTileCreation.NthFibonacciNumber(40));
+			//Debug.Log("30: " + SimulateTileCreation.NthFibonacciNumber(30));
+			//Debug.Log("35: " + SimulateTileCreation.NthFibonacciNumber(35));
+			//Debug.Log("40: " + SimulateTileCreation.NthFibonacciNumber(40));
 
-			//_hdWorker.StartWorking(() =>
-			//{
-			//	Debug.Log("30: " + SimulateTileCreation.NthFibonacciNumber(30));
-			//	Debug.Log("35: " + SimulateTileCreation.NthFibonacciNumber(35));
-			//	Debug.Log("40: " + SimulateTileCreation.NthFibonacciNumber(40));
-			//});
+			_hdWorker.StartWorking(() =>
+			{
+				Debug.Log("30: " + SimulateTileCreation.NthFibonacciNumber(30));
+				Debug.Log("35: " + SimulateTileCreation.NthFibonacciNumber(35));
+				_result = SimulateTileCreation.NthFibonacciNumber(40);
+				Debug.Log("40: " + _result.Value);
+			});
+		}
 
-
+		if (_result.HasValue)
+		{
+			GameObject go = new GameObject(_result.Value.ToString());
+			_result = null;
+			var camera = GameObject.Find("Main Camera");
+			go.transform.SetParent(camera.transform);
 		}
 	}
+
+
 }
